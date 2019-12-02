3ffeadea4d210e5e9cf3e8952c5e333a9cca2fd6
Katherine <47138336+rocka840@users.noreply.github.com>
2019-12-02 08:18:24 +0100

    Add files via upload

--- object.js
+++ object.js
@@ -1,11 +1,11 @@
 class Car {
     constructor(color, speed) {
-        this.color = color,
-        this.speed = speed
+        this.color = color;
+        this.speed = speed;
     }
-};
+}
 
-var carOne = new Car('red', '120');
+var carOne = new Car('Red', '120');
 var carTwo = new Car('Blue', '90');
 
 console.log(carOne);
